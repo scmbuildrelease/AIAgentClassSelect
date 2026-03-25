77e97f77daa590d968b7313a4a886e1cb2827f95
scmbuildrelease <scmbuildrelease@users.noreply.github.com>
2026-03-24 22:08:45 -0700

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,21 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 import os
 import json
 
 app = Flask(__name__)
-DATA_FILE = os.path.join("..", "data", "courses_latest.json")
+CORS(app)
+
+DATA_FILE = "/data/courses_latest.json"
 
 @app.route("/courses")
 def get_courses():
     if os.path.exists(DATA_FILE):
-        with open(DATA_FILE, "r", encoding="utf-8") as f:
-            return jsonify(json.load(f))
+        try:
+            with open(DATA_FILE, "r", encoding="utf-8") as f:
+                return jsonify(json.load(f))
+        except Exception as e:
+            return jsonify({"error": str(e)}), 500
     return jsonify([])
 
 if __name__ == "__main__":
